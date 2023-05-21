c86ea3ed7d570d402c67b06ff14a18f83b53f667
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-05-21 13:13:00 -0300

    Finalizando

---
+++ desafio.kt
@@ -0,0 +1,42 @@
+// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
+
+data class Usuario(val nome: String)
+
+data class ConteudoEducacional(val nome: String, val duracao: Int = 120, val nivel: Nivel)
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
+
+    val inscritos = mutableListOf<Usuario>()
+
+    fun matricular(vararg usuarios: Usuario) {
+        for (u in usuarios) {
+            inscritos.add(u)
+            println("Usuário ${u.nome} adicionado")
+        }
+    }
+}
+
+fun main() {
+    val joao = Usuario("João")
+    val maria = Usuario("Maria")
+    val marcos = Usuario("Marcos")
+
+    val kotlin = ConteudoEducacional(nome = "Kotlin", nivel = Nivel.BASICO)
+    val java = ConteudoEducacional("Java", 240, Nivel.INTERMEDIARIO)
+    val android = ConteudoEducacional("Android", 300, Nivel.AVANCADO)
+
+    val conteudos = listOf<ConteudoEducacional>(kotlin, java, android)
+
+    val formacaoKotlin = Formacao("Formação Kotlin", conteudos, Nivel.INTERMEDIARIO)
+    formacaoKotlin.matricular(joao, maria, marcos)
+
+    println()
+    println("Título: ${formacaoKotlin.nome} (Nível: ${formacaoKotlin.nivel})")
+    println("Matriculados: ")
+    for (u in formacaoKotlin.inscritos) println("${u.nome} ")
+    println("Conteudos: ")
+    for (c in formacaoKotlin.conteudos) println("${c.nome} (${c.duracao} minutos, Nível: ${c.nivel})")
+
+}
